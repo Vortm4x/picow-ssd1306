aef3e63ce73ad6050e5b9bcc0648998e19bc4d51
Maksym Trehubenko <maksym.trehubenko@globallogic.com>
2024-01-29 11:52:01 +0200

    driver code updated

--- examples/main.c
+++ examples/main.c
@@ -15,7 +15,6 @@
 #define SSD1306_I2C_INSTANCE        i2c0
 #define SSD1306_I2C_SDA_PIN         16
 #define SSD1306_I2C_SCL_PIN         17
-#define SSD1306_I2C_ADDRESS         _u(0x3C)
 
 
 void init_display();
@@ -29,27 +28,27 @@ void init_display()
     ssd1306_power_off();
 
     // memory mapping
-    ssd1306_set_horizontal_mem_mode();
+    ssd1306_set_mem_mode_horizontal();
 
     // resolution and layout
     ssd1306_set_display_start_line(0);
-    ssd1306_seg_remap_enable();
-    ssd1306_set_multiplex_ratio(SSD1306_HEIGHT - 1);
-    ssd1306_set_com_out_scan_dir_remapped();
+    ssd1306_seg_remap_on();
+    ssd1306_set_mux_ratio(SSD1306_HEIGHT - 1);
+    ssd1306_com_out_scan_remap_on();
     ssd1306_set_display_offset(0);
-    ssd1306_set_com_pin_config(SSD1306_CMD_FLAG_PIN_CONFIG_ALT, SSD1306_CMD_FLAG_COM_REMAP_OFF);
+    ssd1306_set_com_pin_config_alt_remap_off();
 
     // timing and driving scheme
-    ssd1306_set_dclk_and_freq(SSD1306_DEFAULT_DCLK_RATIO, SSD1306_DEFAULT_OSC_FREQ);
-    ssd1306_set_pre_charge_period(1, 15);
+    ssd1306_set_dclk_div_and_osc_freq(SSD1306_DEFAULT_DCLK_DIV_RATIO, SSD1306_DEFAULT_OSC_FREQ_LEVEL);
+    ssd1306_set_precharge_period(1, 15);
     ssd1306_set_vcomh_deselect_level(SSD1306_VCOMH_DESELECT_LVL_0_83);
 
     // display
-    ssd1306_set_contrast_control(0xFF);
-    ssd1306_follow_display_ram();
-    ssd1306_display_inversion_off();
-    ssd1306_charge_pump_enable();
-    ssd1306_stop_scrolling();
+    ssd1306_set_contrast(0xFF);
+    ssd1306_follow_ram();
+    ssd1306_inversion_off();
+    ssd1306_charge_pump_on();
+    ssd1306_scroll_off();
 
     // set display on
     ssd1306_power_on();
@@ -70,7 +69,7 @@ bool init_all()
     }
 
     bi_decl(bi_2pins_with_func(SSD1306_I2C_SDA_PIN, SSD1306_I2C_SCL_PIN, GPIO_FUNC_I2C));
-    ssd1306_init_i2c(SSD1306_I2C_INSTANCE, SSD1306_I2C_ADDRESS, SSD1306_I2C_SDA_PIN, SSD1306_I2C_SCL_PIN);
+    ssd1306_init_i2c(SSD1306_I2C_INSTANCE, SSD1306_I2C_SDA_PIN, SSD1306_I2C_SCL_PIN);
 
     init_display();
 
@@ -79,8 +78,8 @@ bool init_all()
 
 void render(uint8_t ram_buffer[]) 
 {
-    ssd1306_set_column_addr(0, SSD1306_WIDTH - 1);
-    ssd1306_set_page_addr(0, SSD1306_PAGE_COUNT - 1);
+    ssd1306_set_col_address(0, SSD1306_WIDTH - 1);
+    ssd1306_set_page_address(0, SSD1306_PAGE_COUNT - 1);
 
     ssd1306_send_data(ram_buffer, SSD1306_RAM_BUFF_SIZE);
 }
